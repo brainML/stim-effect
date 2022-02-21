651b697aae2184a695c418c378d790af26adc18f
jennwilliams <jlw1@cs.cmu.edu>
2022-02-21 17:36:22 -0500

    Generalize to any template space nifti file

--- data/preprocessing/A1_downsampled_MNI_template_to_2mm.sh
+++ data/preprocessing/A1_downsampled_MNI_template_to_2mm.sh
@@ -1,3 +1,3 @@
-input=${MNI_file} #ex. for Courtois NeuroMod MNI_file="../mni152_icbm_2009c_nlin_asym_1mm_t1_brain"
-flirt -in ${input} -ref ${input} -applyxfm -applyisoxfm 2 -nosearch -out ${MNI_file}_downsampled_to_2mm
+input=${T1_file} #ex. for Courtois NeuroMod T1_file="../mni152_icbm_2009c_nlin_asym_1mm_t1_brain"
+flirt -in ${input} -ref ${input} -applyxfm -applyisoxfm 2 -nosearch -out ${T1_file}_downsampled_to_2mm
 
